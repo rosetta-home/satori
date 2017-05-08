af1bbeb36b4f94bd903be6a947acd0b0ec41c1f2
Christopher Coté <Christopher Cot>
2017-05-08 15:49:20 -0500

    update for hex add license

--- mix.exs
+++ mix.exs
@@ -7,31 +7,38 @@ defmodule Satori.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger, :ssl, :crypto, :websocket_client],
      mod: {Satori.Application, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:websocket_client, "~> 1.2"},
       {:poison, "~> 3.1"}
+    ]
+  end
+
+  def description do
+    """
+    Websocket Client for Satori (https://www.satori.com)
+    """
+  end
+
+  def package do
+    [
+      name: :satori,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Christopher Steven Coté"],
+      licenses: ["Apache License 2.0"],
+      links: %{"GitHub" => "https://github.com/NationalAssociationOfRealtors/satori",
+          "Docs" => "https://github.com/NationalAssociationOfRealtors/satori"}
     ]
   end
 end
